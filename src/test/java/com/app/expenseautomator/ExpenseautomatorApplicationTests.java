483cf7a7ccf02d6b584917594f35fbf2b0cbeaa1
parasuramvinayagav <parasuramvinayagav@gmail.com>
2026-07-16 20:51:24 +0530

    Initial commit

---
+++ src/test/java/com/app/expenseautomator/ExpenseautomatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.expenseautomator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseautomatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
